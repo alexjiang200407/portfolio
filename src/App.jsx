8e002cfe02b87a23a54f98b04516bcb81b9b7444
alex jiang <alexjiang201507@gmail.com>
2025-04-25 20:50:30 +1000

    david statue

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { OrbitControls } from '@react-three/drei';
 import './App.css'
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useEffect, useState } from 'react';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
+
+function Model(props) {
+  let [obj, setObj] = useState(null)
+
+  useEffect(() => {
+    new OBJLoader().load('Untitled.obj', obj => {
+      // obj.rotation.x = -Math.PI / 2;
+      // obj.rotation.z = Math.PI / 2;
+      // obj.rotation.y = Math.PI;
+      // obj.scale.set(0.001, 0.001, 0.001)
+      setObj(obj)
+    });
+  }, [])
+
+  useFrame(() => {
+    if (obj) {
+      // obj.rotation.x += 0.02
+    }
+
+  })
+
+
+  if (obj) {
+    return <primitive
+      object={obj} position={[0, 0, 0]} {...props} />;
+  }
+  if (!obj) return null
+
+  return <primitive object={obj} {...props} />
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Canvas
+      dpr={[1, 2]} style={{ height: '100vh', width: '100vw' }}
+      camera={{ position: [0, 0, 0] }}
+    >
+      <color attach="background" args={['black']} />
+      <Model />
+      <directionalLight position={[-5, 0, -5]} intensity={2} />
+      {/* <directionalLight position={[5, 2, 5]} intensity={1} /> */}
+    </Canvas>
+  );
 }
 
 export default App
