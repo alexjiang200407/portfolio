8f6c03c9b9458dea60eb6c04a8aaafb7c0ed82bb
alex jiang <alexjiang201507@gmail.com>
2025-04-26 02:57:50 +1000

    Loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,132 @@
-import { ContactShadows, Environment, OrbitControls, Stats } from '@react-three/drei';
+import { Stats } from '@react-three/drei';
 import './App.css'
 import * as THREE from 'three'
-import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber';
-import { useEffect, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useEffect, useRef, useState } from 'react';
 import { Bloom, EffectComposer, N8AO } from "@react-three/postprocessing"
 import { OBJLoader } from 'three-stdlib'
+import LoadingScreen from './Loader';
 
-function Model(props) {
-  const obj = useLoader(OBJLoader, '/rapid.obj')
+
+function Model({ assetsMap }) {
+  const obj = assetsMap['rapid.obj']
   const material = new THREE.MeshStandardMaterial({
-    normalMap: useLoader(THREE.TextureLoader, '/material0_normal.jpg'),
-    aoMap: useLoader(THREE.TextureLoader, '/material0_occlusion.jpg'),
+    normalMap: assetsMap['material0_normal.jpg'],
+    aoMap: assetsMap['material0_occlusion.jpg'],
   });
-
   obj.children[0].material = material;
-    
-  return <primitive object={obj} {...props} />
+
+  return <primitive object={obj} />
 }
 
-const showStats = true;
+const showStats = process.env.NODE_ENV !== 'production';
 const cursor = {
-  x:0,
-  y:0
+  x: 0,
+  y: 0
 };
 
 function FillLight() {
-  const {scene} = useThree()
-  const fillLight = new THREE.PointLight(0xffffff, 5, 4, 3) 
-
-  useEffect(() => {
-    scene.add(fillLight)
-  }, [])
+  const fillLight = useRef();
 
   useFrame((state, delta) => {
     const parallaxY = cursor.y
-    fillLight.position.y -= ( parallaxY *9 + fillLight.position.y -2) * delta
+    fillLight.current.position.y -= (parallaxY * 9 + fillLight.current.position.y - 2) * delta
 
     const parallaxX = cursor.x
-    fillLight.position.x += (parallaxX *8 - fillLight.position.x) * 2 * delta
+    fillLight.current.position.x += (parallaxX * 8 - fillLight.current.position.x) * 2 * delta
 
-    state.camera.position.z -= (parallaxY/3 + state.camera.position.z) * 2 * delta
-    state.camera.position.x += (parallaxX/3 - state.camera.position.x) * 2 * delta
+    state.camera.position.z -= (parallaxY / 3 + state.camera.position.z) * 2 * delta
+    state.camera.position.x += (parallaxX / 3 - state.camera.position.x) * 2 * delta
   })
 
-  return null
+  return <pointLight
+    color={0xffffff}
+    intensity={5}
+    distance={4}
+    decay={3}
+    ref={fillLight}
+  />
+}
+
+const assets = {
+  'rapid.obj': OBJLoader,
+  'material0_normal.jpg': THREE.TextureLoader,
+  'material0_occlusion.jpg': THREE.TextureLoader
 }
 
 
-function App() {
-  useEffect(() => {
-    // window.addEventListener('keydown', handleKeyPress)
 
-    document.addEventListener('mousemove', (event) => {
-      event.preventDefault()
-      cursor.x = event.clientX / window.innerWidth -0.5
-      cursor.y = event.clientY / window.innerHeight -0.5
-    }, false)
-  
-
-  }, [])
-  
+function Movie({ assetsMap }) {
   return (
-    <Canvas
-      dpr={[1, 2]} style={{ height: '100vh', width: '100vw' }}
-      camera={{ position: [0, 0, 0] }}
-      gl={{
-        physicallyCorrectLights: true,
-        outputEncoding: THREE.sRGBEncoding,
-        toneMapping: THREE.ACESFilmicToneMapping,
-      }}
-    >
-      {showStats && <Stats />}
-      <color attach="background" args={['black']} />
-      <Model />
+    <group>
+      <Model assetsMap={assetsMap} />
       <FillLight />
-      <spotLight  
+      <spotLight
         position={[-5, 0, -5]}
         angle={0.3}
         penumbra={0.5}
         intensity={3.5}
         castShadow
       />
-      {/* <Environment files="/adamsbridge.hdr" /> */}
-      <EffectComposer>
-        <N8AO color="black" aoRadius={2} intensity={1.15} />
-        <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={1} />
-      </EffectComposer>
-    </Canvas>
+    </group>
+  )
+}
+
+
+function App() {
+  const [assetsMap, setAssetMap] = useState(null);
+
+  useEffect(() => {
+    Promise.all(Object.entries(assets).map(([key, loader]) => new Promise(resolve => new loader()
+      .load(key, asset => resolve({ [key]: asset }))))
+    )
+      .then(data => new Promise(resolve => setTimeout(() => resolve(data), 1000)))
+      .then(data => {
+        setAssetMap(data.reduce((acc, obj) => {
+          Object.entries(obj).forEach(([a, b]) => {
+            acc[a] = b;
+          });
+          return acc;
+        }, {}))
+      })
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener('mousemove', (event) => {
+      event.preventDefault()
+      cursor.x = event.clientX / window.innerWidth - 0.5
+      cursor.y = event.clientY / window.innerHeight - 0.5
+    })
+
+    document.addEventListener('focus', () => {
+      cursor.x = 0
+      cursor.y = 0
+    })
+
+  }, [])
+
+  return (
+    <div>
+      <LoadingScreen hidden={assetsMap !== null}>
+        <Canvas
+          dpr={[1, 2]} style={{ height: '100vh', width: '100vw' }}
+          camera={{ position: [0, 0, 5] }}
+          gl={{
+            physicallyCorrectLights: true,
+            outputEncoding: THREE.sRGBEncoding,
+            toneMapping: THREE.ACESFilmicToneMapping,
+          }}
+        >
+          {showStats && <Stats />}
+          <color attach="background" args={['black']} />
+          <Movie assetsMap={assetsMap} />
+          <EffectComposer>
+            <N8AO color="black" aoRadius={2} intensity={1.15} />
+            <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={1} />
+          </EffectComposer>
+        </Canvas>
+      </LoadingScreen>
+    </div>
   );
 }
 
---
+++ src/Loader.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import './Loader.css'
+
+function LoadingScreen({ hidden, children }) {
+  const [transitionDone, setTransitionDone] = useState(false);
+
+  useEffect(() => {
+    if (hidden) {
+      setTransitionDone(false); // Wait for transition to end before showing children
+    }
+  }, [hidden]);
+
+  const handleTransitionEnd = () => {
+    if (hidden) {
+      setTransitionDone(true);
+    }
+  };
+  return (
+    <div>
+      <div
+        id='loader'
+        className={ hidden? 'hidden' : ''}
+        onTransitionEnd={handleTransitionEnd}
+      >
+        <div className="blobs">
+          <div className="blob-center"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+              <feBlend in="SourceGraphic" in2="goo" />
+            </filter>
+          </defs>
+        </svg>
+        <p id='loading-label'>Loading Assets</p>
+      </div>
+      {transitionDone && children}
+    </div>
+  );
+}
+
+export default LoadingScreen
